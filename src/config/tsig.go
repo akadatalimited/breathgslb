c9967be7de9ff8f9fb8da2b67a1b53b998ef4502
Andrew Smalley <andrew@csv2.co.uk>
2025-08-29 02:52:15 +0100

    Collect TSIG secrets globally and enforce on listeners

---
+++ src/config/tsig.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/miekg/dns"
+)
+
+// TSIGSecretMap builds a map of TSIG key names to secrets from all configured zones.
+func TSIGSecretMap(cfg *Config) map[string]string {
+	secrets := make(map[string]string)
+	if cfg == nil {
+		return secrets
+	}
+	for _, z := range cfg.Zones {
+		if z.TSIG == nil {
+			continue
+		}
+		for _, k := range z.TSIG.Keys {
+			name := dns.Fqdn(strings.ToLower(k.Name))
+			secrets[name] = k.Secret
+		}
+	}
+	return secrets
+}
